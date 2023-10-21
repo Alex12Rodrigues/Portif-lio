b45470781bcc752149ad8db17a04fdecbf435ef1
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2023-10-20 21:13:05 -0300

    Primeiro safe

---
+++ js/script.js
@@ -0,0 +1,29 @@
+alert ("Fala povo")
+
+// let n1 = prompt (`Qual o maior time brasileiro:
+// A) São Paulo
+// B) Corinthians
+// C) Santos
+// D) Palmeiras` ) 
+
+
+
+
+
+// if (n1 == "a") {
+//   alert ("Certo")
+// }
+// else if(n1 == "A"){
+//   alert ("Boa")
+// }
+// else if(n1 == "SP"){
+//   alert ("Cria")
+// }
+// else if(n1 == "Rato"){
+//   alert ("RAAAAAAAAAAAAAAATOOOOOOOOOO")
+// }
+// else{
+//   alert (`Respost errado, reposta certa: São Paulo`)
+//   // window.location.href = "https://www.google.com/search?sca_esv=560228990&rlz=1C1GCEU_pt-BRBR979BR979&q=faz+o+l&tbm=isch&source=lnms&sa=X&sqi=2&ved=2ahUKEwiLl7a3lPmAAxVcEbkGHbzpDy4Q0pQJegQIDRAB&biw=1920&bih=961&dpr=1"
+// }
+
